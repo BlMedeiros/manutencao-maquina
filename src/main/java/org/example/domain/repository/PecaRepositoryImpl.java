2e1bece3749b235af9c17fa1a37ec25f4c73499f
bruno_l_medeiros <brunoluismedeiros.08@gmail.com>
2025-10-20 18:44:22 -0300

    fix: corrige erro ao cadastrar uma peça

--- src/main/java/org/example/domain/repository/PecaRepositoryImpl.java
+++ src/main/java/org/example/domain/repository/PecaRepositoryImpl.java
@@ -5,6 +5,7 @@ import org.example.domain.repository.connection.ConnectionDatabase;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PecaRepositoryImpl implements PecaRepository {
@@ -13,7 +14,7 @@ public class PecaRepositoryImpl implements PecaRepository {
     public String cadastrarPeca(PecasReposicao pecasReposicao) {
         String insertQuery = """
                 INSERT INTO peca(nome,estoque)
-                VALUES ?,?
+                VALUES (?,?)
                 """;
 
         try (Connection con = ConnectionDatabase.conectar();
@@ -37,20 +38,19 @@ public class PecaRepositoryImpl implements PecaRepository {
     @Override
     public boolean buscarPecaExistente(PecasReposicao pecasReposicao) {
         String selectQuery = """
-                SELECT nome,estoque
+                SELECT nome
                 FROM peca
-                WHERE nome = ? AND estoque = ?
+                WHERE nome = ?
                 """;
 
         try (Connection con = ConnectionDatabase.conectar();
              PreparedStatement stmt = con.prepareStatement(selectQuery)) {
 
             stmt.setString(1, pecasReposicao.getNome());
-            stmt.setDouble(2, pecasReposicao.getEstoque());
 
-            int linhasAfetadas = stmt.executeUpdate();
+            ResultSet rs = stmt.executeQuery();
 
-            if (linhasAfetadas > 0) {
+            if (rs.next()) {
                 return true;
             } else {
                 return false;
