1336eee4b302d3dcf98a9161b8cf5e7c09ec3291
bruno_l_medeiros <brunoluismedeiros.08@gmail.com>
2025-10-20 20:04:55 -0300

    feat: adiciona método de listagem de peças

--- src/main/java/org/example/domain/repository/PecaRepository.java
+++ src/main/java/org/example/domain/repository/PecaRepository.java
@@ -2,9 +2,13 @@ package org.example.domain.repository;
 
 import org.example.domain.model.PecasReposicao;
 
+import java.util.List;
+
 public interface PecaRepository {
 
     String cadastrarPeca(PecasReposicao pecasReposicao);
 
     boolean buscarPecaExistente(PecasReposicao pecasReposicao);
+
+    List<PecasReposicao> listarPecas();
 }
